88b22bfa8e29d700fd589b56cbdd48a2a4cf1d6d
Brian Beard <brian.a.beard@gmail.com>
2018-12-14 15:55:36 -0600

    WIP

--- src/components/Week.vue
+++ src/components/Week.vue
@@ -10,44 +10,86 @@
           {{ selected.month }} {{ selected.date }}<span class="text-grey-light" v-if="selected.year !== (new Date()).getFullYear()">, {{ selected.year }}</span>
         </span>
       </div>
-      <div class="select-none font-mono flex justify-between items-center">
-        <div 
-          class="text-grey-light text-xs font-normal hover:text-green mr-2" 
-          :class="[!selected.isToday ? 'cursor-pointer': 'text-green font-bold']"
-          @click="changeSelection(new Date())" 
-          title="Go to today"
-        >
-          today
-        </div>
-        <div class="cursor-pointer m-1" @click="changeWeek(-1)" title="Previous Week">&lt;</div>
-        <div class="cursor-pointer m-1" @click="changeWeek(1)" title="Next Week"> &gt;</div>
+      <div class="select-none font-mono w-1/6 flex justify-around justify-center items-center text-grey">
+        <div class="cursor-pointer hover:text-grey-darker" @click="changeWeek(-1)" title="Previous Week">&lt;</div>
+        <div class="cursor-pointer hover:bg-grey-darker rounded-full bg-grey w-1 h-1" @click="changeSelection(new Date())" title="Go to today"></div>
+        <div class="cursor-pointer hover:text-grey-darker" @click="changeWeek(1)" title="Next Week"> &gt;</div>
       </div>
     </div>
     
     <!-- Week -->
     <div class="flex justify-between mt-2 font-sans leading-loose font-medium select-none">
-      <div v-for="(d,i) in week" @click="changeSelection(d.date)" :key="i" class="w-12 flex flex-col items-center justify-between cursor-pointer text-grey-darkest">
+      <div v-for="(d,i) in week" @click="changeSelection(d.date)" :key="i" class="w-12 flex flex-col items-center cursor-pointer text-grey-darker">
         <div class="text-grey">{{ d.day }}</div>
-        <div 
-          class="h-8 w-8 flex items-center justify-center text-sm font-bold rounded-full" 
-          :class="[
-            isSelected(d.date)? isToday(d.date)? 'text-white rounded bg-green' : 'text-white bg-green' : isToday(d.date)? 'border border-green' : '',
-          ]"
-        >
-          {{ d.date.getDate() }}
-        </div>
+          <div 
+            class="h-8 w-8 flex items-center justify-center text-sm font-bold rounded-full mt-3" 
+            :class="[
+              isSelected(d.date) ? 'text-white bg-green' : '',
+              isToday(d.date) ? 'border border-green' : '',
+            ]"
+          >
+            {{ d.date.getDate() }}
+          </div>
+          <div class="flex flex-wrap text-green justify-center text-xl">
+            <span v-for="(m,idx) in dayMeals(d.date)" :key="idx" class="w-1 h-1 rounded-full bg-green m-px mt-2"></span>
+          </div>
+        
       </div>
     </div>
 
     <!-- List -->
-    <div class="mt-8">
-      <div v-for="(meal, index) in selectedMeals" :key="index">
-        {{meal.name}}
+    <div class="mt-10" v-if="showMealForm">
+      <form class="w-full max-w-md" @submit.prevent>
+        <div class="flex flex-wrap -mx-3">
+          <div class="w-full md:w-1/2 px-3 md:mb-0">
+            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-name">Meal Name</label>
+            <input 
+              v-model="newMeal" 
+              class="appearance-none block w-full bg-grey-lightest border border-grey-lighter text-grey-darker rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:text-green-darker" 
+              :class="{'border-red': mealNameInvalid }"
+              id="grid-name" 
+              type="text"
+              @keypress.enter="handleAddMeal"
+              v-focus
+            >
+            <p v-if="mealNameInvalid" class="text-red text-xs italic">Please fill out this field.</p>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div v-else class="mt-8">
+      <div v-for="(meal, index) in selectedMeals" :key="index" class="flex text-lg text-grey-darkest justify-between p-6 border-b border-grey-lighter mb-2 rounded hover:border-grey">
+        <div class="">{{meal.name}}</div>
+        <div class="flex items-center justify-between">
+          <div class="flex items-center text-xs text-green mr-2 cursor-pointer select-none" @click="handleEditMeal(meal, index)">edit</div>
+          <div class="flex items-center justify-center cursor-pointer text-xs p-1 rounded-full bg-red-dark text-white select-none" @click="handleRemoveMeal(meal, index)">remove</div>
+        </div>
       </div>
     </div>
+    <div class=" w-full flex lg:justify-center mt-10">
+      <button v-if="showMealForm" class="focus:outline-none hover:text-green w-full lg:w-1/4 mb-5 mt-5" @click="hideForm">
+        Cancel
+      </button>
+      <button @click="handleAddMeal" class="w-full lg:w-1/4 border bg-green hover:bg-white hover:border-green hover:text-green text-white py-3 px-4 rounded focus:outline-none focus:border-green-dark focus:shadow-inner" type="button">
+        <span v-if="editMode">Save</span>
+        <span v-else>Add Meal</span>
+
+      </button>
+
+    </div>
+    
     
   </div>
 </template>
+
+<style>
+  .week-slider-enter {
+    transform: translate(-100%, 0);
+  }
+  .week-slider-enter-active, .week-slider-leave-active {
+    transition: 1s;
+  }
+</style>
 
 <script>
 export default {
@@ -67,6 +109,57 @@ export default {
       const selectedDate = this.$store.state.selectedDate
       return date.getDate() == selectedDate.getDate() && date.getMonth() == selectedDate.getMonth() && date.getFullYear() == selectedDate.getFullYear();
     },
+    handleAddMeal() {
+      // Show the form if not shown
+      if (!this.showMealForm) {
+        this.showMealForm = true
+        return
+      }
+
+      // Check for empty state
+      if(!this.newMeal) { 
+        this.mealNameInvalid = true
+        return
+      }
+
+      if(this.editMode) {
+        // Edit Instead
+        console.log(this.newMeal, this.editIdx, this.selectedDateAtEdit)
+        this.$store.commit("editmeal", this.newMeal, this.editIdx, this.selectedDateAtEdit)
+        
+      } else {
+        // Add to store
+        this.$store.commit("addmeal", this.newMeal)
+      }
+
+      this.hideForm()
+    },
+    handleEditMeal(m,i) {
+      console.log(m, i)
+      this.selectedDateAtEdit = this.$store.state.selectedDate
+      this.editMode = true
+      this.newMeal = m.name
+      this.showMealForm = true
+      this.mealNameInvalid = false
+      this.editIdx = i
+    },
+    handleRemoveMeal(m, i) {
+      this.$store.commit("removemeal", m)
+    },
+    dayMeals(d) {
+      const res = this.$store.state.plan.filter(p => {
+        return d.getDate() == p.date.getDate() && d.getMonth() == p.date.getMonth() && d.getFullYear() == p.date.getFullYear();
+      }).map(m => m.meals )
+      return res[0]
+    },
+    hideForm() {
+      // Reset local state
+      this.mealNameInvalid = false
+      this.newMeal = ''
+      this.showMealForm = false
+      this.editIdx = null
+      this.editMode = false
+    }
   },
   computed: {
     selected() {
@@ -100,12 +193,25 @@ export default {
       return days
     },
     selectedMeals() {
-      const { selectedDate: s } = this.$store.state
-      
-      const res = this.$store.state.plan.filter(p => {
-        return s.getDate() == p.date.getDate() && s.getMonth() == p.date.getMonth() && s.getFullYear() == p.date.getFullYear();
-      }).map(m => m.meals )
-      return res[0]
+      return this.dayMeals(this.$store.state.selectedDate)
+    }
+    
+  },
+  data() {
+    return {
+      selectedDateAtEdit: new Date(),
+      editIdx: null,
+      showMealForm: false,
+      newMeal: "",
+      mealNameInvalid: false,
+      editMode: false
+    }
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
     }
   }
 }
--- src/store.js
+++ src/store.js
@@ -18,7 +18,7 @@ export default new Vuex.Store({
   state: {
     selectedDate: new Date(),
     locale: "en-US",
-    plan: [{ date: new Date(), meals: [{ name: "meal" }, { name: "meal 2" }] }]
+    plan: [{ date: new Date(), meals: [{ name: "Oatmeal" }, { name: "Tikka Masala" }] }]
   },
   mutations: {
     changeweek(store, direction) {
@@ -32,7 +32,33 @@ export default new Vuex.Store({
     },
     changeselection(store, date) {
       store.selectedDate = new Date(date);
+    },
+    addmeal(store, name) {
+      const date = store.selectedDate;
+
+      const i = store.plan.findIndex(p => isSameDate(date, p.date) )
+
+      if (i === -1) {
+        store.plan.push({ date, meals: [{ name }] });
+      } else {
+        store.plan[i].meals.push({ name });
+      }
+    },
+    removemeal(store, meal) {
+      const i = store.plan.findIndex(p => isSameDate(store.selectedDate, p.date));
+      store.plan[i].meals = store.plan[i].meals.filter(m => m !== meal)
+    },
+    editmeal(store, newName, mealIdx, d) {
+      console.log(d, mealIdx, newName);
+      const i = store.plan.findIndex(p => { 
+        
+        return isSameDate(d, p.date) })
+      store.plan[i].meals[mealIdx].name = newName
     }
   },
   actions: {}
 });
+
+const isSameDate = (d1, d2) => {
+  return d1.getDate() == d2.getDate() && d1.getMonth() == d2.getMonth() && d1.getFullYear() == d2.getFullYear();
+}
